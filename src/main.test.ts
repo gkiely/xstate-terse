a8d929e689e6ddaf9e3f60e32edd8ed2f866c75b
Grant Kiely <grant.kiely@gmail.com>
2022-11-14 14:28:09 -0600

    Initial commit

---
+++ src/main.test.ts
@@ -0,0 +1,50 @@
+import { machine } from './main';
+
+it('generates a sane default machine', () => {
+  // First define your context, states and events
+  const requestMachine = machine({
+    context: {
+      hello: 'world',
+    },
+    states: ['idle', 'loading', 'error'],
+    events: [
+      {
+        type: 'route',
+      },
+    ],
+  } as const);
+
+  // Then define your transitions and side effects
+  const fn = () => Math.random() * 10 > 5;
+  requestMachine.matches('idle').on('route').target('loading');
+  requestMachine.matches('idle').target('loading', fn).target('error');
+
+  expect(requestMachine.getSnapshot()).toEqual({
+    initial: 'idle',
+    preserveActionOrder: true,
+    predictableActionArguments: true,
+    context: {
+      hello: 'world',
+    },
+    states: {
+      idle: {
+        always: [
+          {
+            target: 'loading',
+            cond: fn,
+          },
+          {
+            target: 'error',
+          },
+        ],
+        on: {
+          route: {
+            target: 'loading',
+          },
+        },
+      },
+      loading: {},
+      error: {},
+    },
+  });
+});
---
+++ src/main.ts
@@ -0,0 +1,93 @@
+// type JSONValue = string | number | boolean | null | { [x: string]: JSONValue } | Array<JSONValue>;
+// type JSONObject = Record<string, JSONValue>;
+type JSValue = string | number | boolean | null | undefined | { [x: string]: JSValue } | Array<JSValue>;
+type JSObject = Record<string, JSValue>;
+
+// import { MachineConfig } from 'xstate';
+// type Result = MachineConfig<Options['context'], Record<string, unknown>, { type: '' }>;
+
+export const assertType = <T>(_: unknown): asserts _ is T => {};
+
+type Options = {
+  context: JSObject;
+  states: readonly string[];
+  events: readonly { type: string; payload?: JSObject | undefined }[];
+};
+
+export const machine = <
+  O extends Options,
+  Context extends Options['context'],
+  States extends O['states'][number],
+  Events extends O['events'][number]['type']
+>(
+  options: O
+) => {
+  const { context, states: stateKeys } = options;
+  type StateNodeEvent = Record<
+    Events,
+    {
+      target: States;
+      actions?: () => {};
+    }
+  >;
+  type StateNode = Record<
+    States,
+    {
+      on: StateNodeEvent;
+      always: {
+        target: States;
+        actions?: () => {};
+        cond?: (c?: Context) => boolean;
+      }[];
+    }
+  >;
+
+  const states = stateKeys.reduce((prev, curr) => {
+    return {
+      ...prev,
+      [curr]: {},
+    };
+  }, {} as StateNode);
+
+  const snapshot = {
+    initial: stateKeys.at(0),
+    preserveActionOrder: true,
+    predictableActionArguments: true,
+    context,
+    states,
+  };
+
+  const methods = {
+    getSnapshot: () => snapshot,
+    matches: (state: States) => {
+      return {
+        target: (nextState: States, cond?: (c?: Context) => boolean) => {
+          const stateNode = snapshot.states[state];
+          if (!stateNode.always) {
+            stateNode.always = [];
+          }
+          stateNode.always.push({
+            target: nextState,
+            ...(cond && { cond }),
+          });
+          return methods.matches(state);
+        },
+        on: (event: Events) => {
+          return {
+            target: (target: States) => {
+              const stateNode = snapshot.states[state];
+              stateNode.on = {
+                [event]: {
+                  target,
+                },
+              } as StateNodeEvent;
+            },
+          };
+        },
+      };
+    },
+  };
+
+  return methods;
+  /// TODO add satisfies type Result
+};
